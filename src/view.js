6039c1c726dbb9330a8178583938c8c7e73e06fc
Musa-94 <x.m.m.o@mail.ru>
2019-12-14 00:03:16 +0400

    interface generate people

--- src/view.js
+++ src/view.js
@@ -1,16 +1,34 @@
 class View {
     constructor() {
         this._root = document.getElementById('root');
+        this._wrapperGenerator = null;
     }
 
     init = () => {
         const wrapper = document.createElement('div');
         wrapper.classList.add('wrapper');
         
-        const wrapperGenerator = document.createElement('div');
-        wrapperGenerator.classList.add('wrapper__generator');
-        wrapper.append(wrapperGenerator);
+        this._wrapperGenerator = document.createElement('div');
+        this._wrapperGenerator.classList.add('wrapper__generator');
+        wrapper.append(this._wrapperGenerator);
 
+        const generatorInputs = this.createInputs([
+            {type: 'number', placeholder: '2', required: true }, 
+            {type: 'number', placeholder: 'min age', required: true }, 
+            {type: 'number', placeholder: 'max age', required: true }, 
+            {type: 'text', placeholder: 'min health', required: true },
+            {type: 'text', placeholder: 'max health', required: true },
+            {type: 'number', placeholder: 'min money', required: true },
+            {type: 'number', placeholder: 'max money', required: true },
+            {type: 'number', placeholder: 'min offenses', required: true },
+            {type: 'number', placeholder: 'max offenses', required: true }
+        ]);
+
+        this._wrapperGenerator.append(...generatorInputs);
+
+        const buttonGenerator = this.buttonCreate('generate', 'Generate');
+        this._wrapperGenerator.append(buttonGenerator);
+        
         const wrapperSimulator = document.createElement('div');
         wrapperSimulator.classList.add('wrapper__simulator');
         wrapper.append(wrapperSimulator);
