2a29357e102ddce116a1892538f4c242014ed7c4
Musa-94 <x.m.m.o@mail.ru>
2019-12-13 13:54:18 +0400

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es6': true,
+        'node': true
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
+    },
+    "parser": "babel-eslint",
+    'rules': {
+        'indent': [
+            'error',
+            4,
+            { "SwitchCase": 1 }
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import View from './View';
 const init = () => {
     const view = new View();
 
+  
+  
     view.init();
-}
+};
 
 init();
