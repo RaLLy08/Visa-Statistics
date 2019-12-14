5d7a1b7fc9dad18616b53f54253f037d77a546f8
RaLLy08 <uytemataaero@mail.ru>
2019-12-14 23:21:51 +0400

    setting random comleted

--- src/Controller.js
+++ src/Controller.js
@@ -10,6 +10,16 @@ class Controller {
     init = () => {
         this._view.init();
     }
+
+    setRandomConfig = (value, minAge, maxAge, minHealth, maxHealth, minMoney, maxMoney, minOffenses, maxOffenses) => {
+        for (let i = 0; i >= value; index++) {
+            age = Math.floor(Math.random() * (maxAge - minAge) + minAge);
+            health = Math.floor(Math.random() * (maxHealth - minHealth) + minHealth);
+            money = Math.floor(Math.random() * (maxMoney - minMoney) + minMoney);
+            offenses = Math.floor(Math.random() * (maxOffenses - minOffenses) + minOffenses);
+            this.createPerson(age, health, money, offenses);
+        }
+    }
 }
 
 export default Controller;
